6b834d8389d09ebc356b8581fbc88880985767f2
rene_sanguenza <rene@venturedrake.com>
2024-01-22 14:51:38 +0800

    debugging PWA

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import {defineConfig, loadEnv} from "vite";
 import {createVuePlugin} from "vite-plugin-vue2";
 import Components from 'unplugin-vue-components/vite'
 import {VuetifyResolver} from "unplugin-vue-components/resolvers";
+import { VitePWA } from 'vite-plugin-pwa'
 
 const HOST = "0.0.0.0"
 const REPLACEMENT = `${path.resolve(__dirname, './src')}/`
@@ -32,6 +33,27 @@ export default () => {
             ],
         },
         plugins: [
+            VitePWA({ 
+                registerType: 'autoUpdate',
+                manifest: {
+                    name: 'storiesforyou',
+                    short_name: 'storiesforyou',
+                    description: "When you read an online story, you're not just reading it on your own. You're joining a community of readers and authors who are passionate about stories. You can share your thoughts on the story with other readers, and you can even leave comments for the story.",
+                    theme_color: '#f5ecbc',
+                    icons: [
+                      {
+                        src: '/public/img/icons/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                      },
+                      {
+                        src: '/public/img/icons/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                      }
+                    ]
+                  }                 
+            }),
             createVuePlugin(),
             Components({
                 resolvers: [
