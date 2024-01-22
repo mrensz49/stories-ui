f2e35c7d2d3dd87e31bb5452dd3956e8913a2731
rene_sanguenza <rene@venturedrake.com>
2024-01-22 11:52:49 +0800

    update env and vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import path from "path";
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import {createVuePlugin} from "vite-plugin-vue2";
 import Components from 'unplugin-vue-components/vite'
 import {VuetifyResolver} from "unplugin-vue-components/resolvers";
 
 const HOST = "0.0.0.0"
 const REPLACEMENT = `${path.resolve(__dirname, './src')}/`
-
+const env = loadEnv(
+    'all',
+    process.cwd()
+);
+let port = env.VITE_NODE_ENV;
 
 export default () => {
     return defineConfig({
-        base: "./",
+        base: env.VITE_NODE_ENV == 'development' ? "./" : env.VITE_BASE_URL,
         server: {
             host: HOST,
         },
